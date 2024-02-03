b16088bc0fcfa4c4688d57a32420c3d0b4ead3c4
Alexei Torgashov <alexei.torgashov@yahoo.de>
2024-02-03 19:23:47 +0100

    piantor 36 keys

---
+++ keyboards/trgsv/piantor/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/* keyboard uid */
+#define VIAL_KEYBOARD_UID \
+    { 0xDC, 0x6D, 0x9C, 0x42, 0x53, 0x42, 0x75, 0x0F }
+
+#define VIAL_UNLOCK_COMBO_ROWS \
+    { 1, 4 }
+#define VIAL_UNLOCK_COMBO_COLS \
+    { 3, 2 }
+
+/* default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
