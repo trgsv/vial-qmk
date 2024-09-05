7f4dbddb06866ab790022dbd4acb3891fcea03c1
Alexei Torgashov <alexei.torgashov@yahoo.de>
2024-09-05 19:32:04 +0200

    piantor mini vial

---
+++ keyboards/beekeeb/piantor/keymaps/vial_mini/config.h
@@ -0,0 +1,11 @@
+/* keyboard uid */
+#define VIAL_KEYBOARD_UID \
+    { 0xC9, 0x4D, 0xE4, 0xB4, 0x69, 0xC0, 0x07, 0x3B }
+
+#define VIAL_UNLOCK_COMBO_ROWS \
+    { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS \
+    { 1, 4 }
+
+/* default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
